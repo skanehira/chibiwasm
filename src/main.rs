365acf35c3d2b9a7160839d928eba0133e491dfd
skanehira <sho19921005@gmail.com>
2023-03-29 00:05:20 +0900

    refactor: remove unnecessary type annotation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
 use chibiwasm::execution::runtime::Runtime;
-use chibiwasm::execution::value::Value;
 use clap::Parser;
 
 #[derive(Debug, Parser)]
@@ -16,7 +15,7 @@ struct Args {
 fn main() -> Result<()> {
     let args = Args::parse();
     let mut runtime = Runtime::from_file(&args.file)?;
-    let func_args: Vec<Value> = args.func_args.into_iter().map(Value::from).collect();
+    let func_args = args.func_args.into_iter().map(Into::into).collect();
     let result = runtime.call(args.func, func_args)?;
     println!("{:?}", result);
     Ok(())
